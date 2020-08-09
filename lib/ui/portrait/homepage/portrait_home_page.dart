3ceb8e627629bc55d26ce4df18d0193881cb9633
Admin <imlegend103@gmail.com>
2020-08-09 10:56:47 +0700

    fixed homepage

--- lib/ui/portrait/homepage/portrait_home_page.dart
+++ lib/ui/portrait/homepage/portrait_home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:marozi/resources/colors.dart';
 import 'package:marozi/resources/custom_widgets/my_text.dart';
-import 'package:marozi/resources/fonts.dart';
 import 'package:marozi/resources/strings.dart';
 
 class PortraitHomePage extends StatefulWidget {
@@ -27,33 +26,33 @@ class _PortraitHomePageState extends State<PortraitHomePage> {
       resizeToAvoidBottomInset: false,
       body: SafeArea(
         top: true,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: Align(
-                  alignment: Alignment.topCenter,
-                  child: MyText(
-                    text: 'Select mode',
-                    color: Colors.black,
-                    fontSize: 22,
-                    fontWeight: FontWeight.w500,
-                  )),
-            ),
-            Spacer(),
-            _pageView(),
-            _goButton(),
-            Spacer(),
-          ],
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topCenter,
+                child: MyText(
+                  text: 'Select mode',
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              _pageView(),
+              _goButton(),
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget _pageView() {
-    return Container(
-      height: 470,
+    return Expanded(
       child: PageView(
         controller: _pageController,
         scrollDirection: Axis.horizontal,
@@ -66,32 +65,36 @@ class _PortraitHomePageState extends State<PortraitHomePage> {
   }
 
   Widget _portrait() {
-    return Card(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          InkWell(
-            onTap: () {
-              Navigator.of(context).pushNamed(table);
-            },
-            child: ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(8),
-                topRight: Radius.circular(8),
+    return Container(
+      alignment: Alignment.center,
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            InkWell(
+              onTap: () {
+                Navigator.of(context).pushNamed(table);
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  topRight: Radius.circular(8),
+                ),
+                child: Image.asset('assets/images/portrait.png'),
               ),
-              child: Image.asset('assets/images/portrait.png'),
             ),
-          ),
-          Expanded(
-            child: Center(
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.symmetric(vertical: 8),
               child: MyText(
                 text: 'Portrait Mode',
                 color: Colors.black,
                 fontSize: 16,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -113,7 +116,7 @@ class _PortraitHomePageState extends State<PortraitHomePage> {
                     ),
                     child: Image.asset('assets/images/landscape.png')),
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 6.0),
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
                   child: Center(
                     child: MyText(
                       text: 'Landscape Mode',
@@ -131,35 +134,31 @@ class _PortraitHomePageState extends State<PortraitHomePage> {
   }
 
   Widget _goButton() {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 10.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        onPressed: () {
-          Navigator.pushNamed(context, table);
-        },
-        textColor: Colors.white,
-        padding: const EdgeInsets.all(0.0),
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.69,
-          height: 40,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            gradient: LinearGradient(
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              colors: [
-                HexColor('FF7008'),
-                HexColor('FF964A'),
-              ],
-            ),
+    return RaisedButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      onPressed: () {
+        Navigator.pushNamed(context, table);
+      },
+      textColor: Colors.white,
+      padding: const EdgeInsets.all(0.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.69,
+        height: 40,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              HexColor('FF7008'),
+              HexColor('FF964A'),
+            ],
           ),
-          padding: EdgeInsets.all(10.0),
-          child: Text(
-            'Go',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20, fontFamily: fontSFDisplayRegular),
-          ),
+        ),
+        child: Text(
+          'Go',
+          style: TextStyle(fontSize: 20),
         ),
       ),
     );
