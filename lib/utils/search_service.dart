c7477a2406eb1d754eefb1dbad2b2bb677005f60
Mạnh Đạt <imlegend103@gmail.com>
2020-09-10 15:22:02 +0700

    a little ui changes

--- lib/utils/search_service.dart
+++ lib/utils/search_service.dart
@@ -10,7 +10,7 @@ class SearchService {
         .collection('Player')
         .where('player_name', isGreaterThanOrEqualTo: query)
         .where("player_name", isLessThan: query + 'z')
-        .limit(20)
+        .limit(10)
         .get();
 
     return searchField.isEmpty ? null : result;
